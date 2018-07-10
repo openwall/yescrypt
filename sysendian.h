06d1bfcc2dd4a9c1cbc997ec4767b6f5746532b9
Solar Designer <solar@openwall.com>
2018-07-10 17:28:19 +0200

    Dropped the 16-bit encoding/decoding functions, which are unused in yescrypt
    CVS-ID: sysendian.h 1.2

--- sysendian.h
+++ sysendian.h
@@ -30,35 +30,14 @@
 #include <stdint.h>
 
 /* Avoid namespace collisions with BSD <sys/endian.h>. */
-#define be16dec libcperciva_be16dec
-#define be16enc libcperciva_be16enc
 #define be32dec libcperciva_be32dec
 #define be32enc libcperciva_be32enc
 #define be64dec libcperciva_be64dec
 #define be64enc libcperciva_be64enc
-#define le16dec libcperciva_le16dec
-#define le16enc libcperciva_le16enc
 #define le32dec libcperciva_le32dec
 #define le32enc libcperciva_le32enc
 #define le64dec libcperciva_le64dec
 #define le64enc libcperciva_le64enc
-
-static inline uint16_t
-be16dec(const void * pp)
-{
-	const uint8_t * p = (uint8_t const *)pp;
-
-	return (uint16_t)((uint16_t)(p[1]) + ((uint16_t)(p[0]) << 8));
-}
-
-static inline void
-be16enc(void * pp, uint16_t x)
-{
-	uint8_t * p = (uint8_t *)pp;
-
-	p[1] = x & 0xff;
-	p[0] = (x >> 8) & 0xff;
-}
 
 static inline uint32_t
 be32dec(const void * pp)
@@ -104,23 +83,6 @@ be64enc(void * pp, uint64_t x)
 	p[2] = (x >> 40) & 0xff;
 	p[1] = (x >> 48) & 0xff;
 	p[0] = (x >> 56) & 0xff;
-}
-
-static inline uint16_t
-le16dec(const void * pp)
-{
-	const uint8_t * p = (uint8_t const *)pp;
-
-	return (uint16_t)((uint16_t)(p[0]) + ((uint16_t)(p[1]) << 8));
-}
-
-static inline void
-le16enc(void * pp, uint16_t x)
-{
-	uint8_t * p = (uint8_t *)pp;
-
-	p[0] = x & 0xff;
-	p[1] = (x >> 8) & 0xff;
 }
 
 static inline uint32_t
